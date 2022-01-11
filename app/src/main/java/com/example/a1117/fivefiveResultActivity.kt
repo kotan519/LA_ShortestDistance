2c35f412ea6814f3721bb678d2f90f031815bcda
kotan519 <97348625+kotan519@users.noreply.github.com>
2022-01-11 22:36:18 +0900

    更新

--- app/src/main/java/com/example/a1117/fivefiveResultActivity.kt
+++ app/src/main/java/com/example/a1117/fivefiveResultActivity.kt
@@ -53,124 +53,127 @@ class fivefiveResultActivity : AppCompatActivity() {
             val Answer = findViewById<EditText>(R.id.answer).text.toString()
             intent.putExtra("answer", Answer)
             startActivity(intent)
+        }
 
-            val ffResultAct = resultOfAlgorithm.getInstance()
-            fun main(args: Array<String>) {
-                val grid = Grid(P(ffResultAct.startyoko, ffResultAct.starttate), P(ffResultAct.goalyoko, ffResultAct.goaltate)).apply {
-                }
-                Search(grid).execute(weight = 1.0)
-            }
-
-            class Search(val grid: Grid) {
-                val ffResultAct = resultOfAlgorithm.getInstance()
-                fun execute(weight: Double) {
-                    val costs = weightedAsterCost(weight)
-
-                    if (costs == null) {
-                        ffResultAct.UnCorrectResult = "Unreached"
-                        return
-                    } else {
-                        ffResultAct.CorrectResult = costs[grid.goal]!!
-                        return
-                    }
-                }
-
-                private fun weightedAsterCost(weight: Double): Map<P, Int>? {
-                    // h: heuristic function
-                    val h = { n: P -> weight * n.distance(grid.goal) }
-
-                    val costs = mutableMapOf<P, Int>().apply { this[grid.start] = 0 }
-                    val queue = PriorityQueue<PC>().apply { add(PC(grid.start, h(grid.start))) }
-                    while (queue.isNotEmpty()) {
-                        val p = queue.poll().point
-                        if (p == grid.goal) return costs
-                        grid.neighbors(p).forEach { n ->
-                            val cost = costs[p]!! + p.distance(n)
-                            if (costs[n] == null || cost < costs[n]!!) {
-                                costs[n] = cost
-                                queue.add(PC(n, cost + h(n)))
-                            }
-                        }
-                    }
-                    return null // the goal is unreachable
-                }
-            }
-
-            class PC(val point: P, private val cost: Double) : Comparable<PC> {
-                override fun compareTo(other: PC): Int {
-                    return when {
-                        cost == other.cost -> 0
-                        cost < other.cost -> -1
-                        cost > other.cost -> 1
-                        else -> throw  IllegalStateException("$cost <-> ${other.cost}")
-                    }
-                }
-            }
-
-            //5,5
-            class Grid(val start: P, val goal: P) {
-                val ffResultAct = resultOfAlgorithm.getInstance()
-                val states = arrayOf(
-                    arrayOf(ffResultAct.ary00, ffResultAct.ary01, ffResultAct.ary02, ffResultAct.ary03, ffResultAct.ary04),
-                    arrayOf(ffResultAct.ary10, ffResultAct.ary11, ffResultAct.ary12, ffResultAct.ary13, ffResultAct.ary14),
-                    arrayOf(ffResultAct.ary20, ffResultAct.ary21, ffResultAct.ary22, ffResultAct.ary23, ffResultAct.ary24),
-                    arrayOf(ffResultAct.ary30, ffResultAct.ary31, ffResultAct.ary32, ffResultAct.ary33, ffResultAct.ary34),
-                    arrayOf(ffResultAct.ary40, ffResultAct.ary41, ffResultAct.ary42, ffResultAct.ary43, ffResultAct.ary44)
-                )
-
-                fun isValid(n: P): Boolean {
-                    return (n.i in states.indices) && (n.j in states[n.i].indices)
-                }
-
-                fun state(n: P): Int {
-                    check(isValid(n)) { "out of range $n" }
-                    return states[n.i][n.j]
-                }
-
-                fun isEmpty(n: P): Boolean {
-                    return state(n) == 0
-                }
-
-                fun neighbors(n: P): List<P> {
-                    return n.around().filter { isValid(it) && isEmpty(it) }
-                }
-            }
-
-            //Pointclass
-            data class P(val i: Int, val j: Int) {
-                fun around(): Array<P> {
-                    return arrayOf(up(), left(), down(), right())
-                }
-
-                // Manhattan distance
-                fun distance(o: P): Int {
-                    return abs(i - o.i) + abs(j - o.j)
-                }
-
-                fun up(): P {
-                    return P(i - 1, j)
-                }
-
-                fun down(): P {
-                    return P(i + 1, j)
-                }
-
-                fun left(): P {
-                    return P(i, j - 1)
-                }
-
-                fun right(): P {
-                    return P(i, j + 1)
-                }
-
-                override fun toString(): String {
-                    return "($i, $j)"
-                }
-            }
+        backButton.setOnClickListener {
+            finish()
         }
     }
 }
 
+val ffResultAct = resultOfAlgorithm.getInstance()
+fun main(args: Array<String>) {
+    val grid = Grid(P(ffResultAct.startyoko, ffResultAct.starttate), P(ffResultAct.goalyoko, ffResultAct.goaltate)).apply {
+    }
+    Search(grid).execute(weight = 1.0)
+}
+
+class Search(val grid: Grid) {
+    val ffResultAct = resultOfAlgorithm.getInstance()
+    fun execute(weight: Double) {
+        val costs = weightedAsterCost(weight)
+
+        if (costs == null) {
+            ffResultAct.UnCorrectResult = "Unreached"
+            return
+        } else {
+            ffResultAct.CorrectResult = costs[grid.goal]!!
+            return
+        }
+    }
+
+    private fun weightedAsterCost(weight: Double): Map<P, Int>? {
+        // h: heuristic function
+        val h = { n: P -> weight * n.distance(grid.goal) }
+
+        val costs = mutableMapOf<P, Int>().apply { this[grid.start] = 0 }
+        val queue = PriorityQueue<PC>().apply { add(PC(grid.start, h(grid.start))) }
+        while (queue.isNotEmpty()) {
+            val p = queue.poll().point
+            if (p == grid.goal) return costs
+            grid.neighbors(p).forEach { n ->
+                val cost = costs[p]!! + p.distance(n)
+                if (costs[n] == null || cost < costs[n]!!) {
+                    costs[n] = cost
+                    queue.add(PC(n, cost + h(n)))
+                }
+            }
+        }
+        return null // the goal is unreachable
+    }
+}
+
+class PC(val point: P, private val cost: Double) : Comparable<PC> {
+    override fun compareTo(other: PC): Int {
+        return when {
+            cost == other.cost -> 0
+            cost < other.cost -> -1
+            cost > other.cost -> 1
+            else -> throw  IllegalStateException("$cost <-> ${other.cost}")
+        }
+    }
+}
+
+//5,5
+class Grid(val start: P, val goal: P) {
+    val ffResultAct = resultOfAlgorithm.getInstance()
+    val states = arrayOf(
+        arrayOf(ffResultAct.ary00, ffResultAct.ary01, ffResultAct.ary02, ffResultAct.ary03, ffResultAct.ary04),
+        arrayOf(ffResultAct.ary10, ffResultAct.ary11, ffResultAct.ary12, ffResultAct.ary13, ffResultAct.ary14),
+        arrayOf(ffResultAct.ary20, ffResultAct.ary21, ffResultAct.ary22, ffResultAct.ary23, ffResultAct.ary24),
+        arrayOf(ffResultAct.ary30, ffResultAct.ary31, ffResultAct.ary32, ffResultAct.ary33, ffResultAct.ary34),
+        arrayOf(ffResultAct.ary40, ffResultAct.ary41, ffResultAct.ary42, ffResultAct.ary43, ffResultAct.ary44)
+    )
+
+    fun isValid(n: P): Boolean {
+        return (n.i in states.indices) && (n.j in states[n.i].indices)
+    }
+
+    fun state(n: P): Int {
+        check(isValid(n)) { "out of range $n" }
+        return states[n.i][n.j]
+    }
+
+    fun isEmpty(n: P): Boolean {
+        return state(n) == 0
+    }
+
+    fun neighbors(n: P): List<P> {
+        return n.around().filter { isValid(it) && isEmpty(it) }
+    }
+}
+
+//Pointclass
+data class P(val i: Int, val j: Int) {
+    fun around(): Array<P> {
+        return arrayOf(up(), left(), down(), right())
+    }
+
+    // Manhattan distance
+    fun distance(o: P): Int {
+        return abs(i - o.i) + abs(j - o.j)
+    }
+
+    fun up(): P {
+        return P(i - 1, j)
+    }
+
+    fun down(): P {
+        return P(i + 1, j)
+    }
+
+    fun left(): P {
+        return P(i, j - 1)
+    }
+
+    fun right(): P {
+        return P(i, j + 1)
+    }
+
+    override fun toString(): String {
+        return "($i, $j)"
+    }
+}
 
 
 
